b24bba20d225223653b1bff6bcc51ea5a30ceb41
Apryl Smith <gidgetpink@gmail.com>
2023-02-06 21:40:30 -0500

    Created functions for second image and title

--- scripts/flags.js
+++ scripts/flags.js
@@ -1,9 +1,14 @@
 const heading = document.querySelector('.title');
-const pics = document.querySelector('.UsFlags');
-pics.addEventListener('mouseover', changeTitle);
-pics.addEventListener('mouseout', returnTitle);
-pics.addEventListener('mouseover', changeImg);
-pics.addEventListener('mouseout', returnImg);
+const pic1 = document.querySelector('.UsFlags');
+pic1.addEventListener('mouseover', changeTitle);
+pic1.addEventListener('mouseout', returnTitle);
+pic1.addEventListener('mouseover', changeImg1);
+pic1.addEventListener('mouseout', returnImg1);
+const pic2 = document.querySelector('.Ethiopia')
+pic2.addEventListener('mouseover', changeTitle2);
+pic2.addEventListener('mouseout', returnTitle2);
+pic2.addEventListener('mouseover', changeImg2);
+pic2.addEventListener('mouseout', returnImg2);
 
 
 function changeTitle(){
@@ -14,10 +19,28 @@ function returnTitle(){
     document.querySelector('.title').innerHTML = "Countries and Flags";
 }
 
-function changeImg(){
+function changeImg1(){
     document.querySelector('.UsFlags').src ="images/USCountry.webp";
 }
 
-function returnImg(){
+function returnImg1(){
     document.querySelector('.UsFlags').src ="images/AmericanFlag.jpg";
+}
+
+//for second image
+
+function changeTitle2(){
+    document.querySelector('.title').innerHTML = "Ethiopia";
+}
+
+function returnTitle2(){
+    document.querySelector('.title').innerHTML = "Countries and Flags";
+}
+
+function changeImg2(){
+    document.querySelector('.Ethiopia').src ="images/Ethiopia.webp";
+}
+
+function returnImg2(){
+    document.querySelector('.Ethiopia').src ="images/ethiopiaFlag.jpg";
 }
